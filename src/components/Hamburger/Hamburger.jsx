97ffd79a06fba78ec151411c7656ede0a49b5cce
Zenon Marek Raubuć <m.raububc@wp.pl>
2023-05-27 05:22:57 +0200

    Update component Hamburger

--- src/components/Hamburger/Hamburger.jsx
+++ src/components/Hamburger/Hamburger.jsx
@@ -1,16 +1,14 @@
 const Hamburger = () => {
-  const btn = document.querySelector(".nav__hamburger");
-const btnItems = document.querySelectorAll(".nav__hamburger-item");
 
-btn.addEventListener("click", () => {
-  // btn.classList.toggle("circle");
-  btnItems[0].classList.toggle("rotate");
-  btnItems[1].classList.toggle("hidden");
-  btnItems[2].classList.toggle("reverse-rotate");
-});
+  const classes = ['rotate', 'hidden', 'reverse-rotate'] 
+
+  const click = () => {
+    const btnItems = document.querySelectorAll(".hamburger__item");
+    btnItems.forEach ((item, i) => item.classList.toggle(classes[i]))
+  }
 
   return (
-    <button className="hamburger">
+    <button className="hamburger" onClick={click}>
       <span className="hamburger__item"></span>
       <span className="hamburger__item"></span>
       <span className="hamburger__item"></span>
