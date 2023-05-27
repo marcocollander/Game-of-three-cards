dd34a14fd7d22f5e40d46d8654738500ecfcc480
Zenon Marek Raubuć <m.raububc@wp.pl>
2023-05-27 20:03:54 +0200

    Update project structure

---
+++ src/app/App.jsx
@@ -0,0 +1,15 @@
+import Header from '../components/Header/Header';
+import Menu from '../components/Menu/Menu';
+import Hamburger from '../components/Hamburger/Hamburger';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Menu />
+      <Hamburger />
+    </>
+  );
+}
+
+export default App;
