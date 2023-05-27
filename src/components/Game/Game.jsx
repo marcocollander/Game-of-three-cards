a1f7dcdacffb4b8c7d05ff2b72fe7b72cc34864c
Zenon Marek Raubuć <m.raububc@wp.pl>
2023-05-27 22:09:36 +0200

    Update component Game

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -3,11 +3,34 @@ import damapik from '../../assets/damapik.jpg';
 import damakier from '../../assets/damakier.jpg';
 
 const Game = () => {
-  <div className="game">
-    <img className="game__card" src={damakaro} alt="dama karo" />
-    <img className="game__card" src={damapik} alt="dama pik" />
-    <img className="game__card" src={damakier} alt="dama kier" />
-  </div>;
+  return (
+    <>
+      <div className="results">
+        <form className="form" action="/" method="post">
+          <fieldset className="form__fieldset">
+            <legend className="form__legend">Wyniki:</legend>
+
+            <label htmlFor="number_attempts">Ilość prób: </label>
+            <input id="number_attempts" name="number_attempts" />
+
+            <label htmlFor="number_hits">Ilość trafień: </label>
+            <input id="number_hits" name="number_hits" />
+          </fieldset>
+        </form>
+      </div>
+
+      <div className="cards">
+        <img className="cards__card" src={damakaro} alt="dama karo" />
+        <img className="cards__card" src={damapik} alt="dama pik" />
+        <img className="cards__card" src={damakier} alt="dama kier" />
+      </div>
+
+      <div className="buttons">
+        <button className="buttons__btn">Start</button>
+        <button className="buttons__btn">Reset</button>
+      </div>
+    </>
+  );
 };
 
 export default Game;
